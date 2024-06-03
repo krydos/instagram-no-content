56164da730a5f0b4e6bdc021e05c37fd943b79af
Ruslan Bekenev <furyinbox@gmail.com>
2024-06-03 13:39:21 +1000

    Init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Tampermonkey script to hide reels and images from web version of Instagram
+
+
+## Demo
+
+<img src="demo.gif" />
+
+## Motivation
+
+When someone contacts me in the Instagram I get easily distracted by
+all those reels. This userscript just hides all the content and helps
+me to avoid doom scrolling.
---
+++ instagram-no-content.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Instagram No Content
+// @namespace    http://tampermonkey.net/
+// @version      2024-06-03
+// @description  try to take over the world!
+// @author       Ruslan Bekenev <furyinbox@gmail.com>
+// @match        https://www.instagram.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @require      https://cdn.jsdelivr.net/npm/arrive@2.4.1/minified/arrive.min.js
+// @grant        none
+// ==/UserScript==
+
+function deleteElements(selectors) {
+    selectors.map(selector => {
+        Array.from(document.querySelectorAll(selector)).map(e => {
+            e.replaceWith('')
+        })
+    })
+}
+
+(function() {
+    'use strict';
+    deleteElements(['video', 'img']);
+
+    document.arrive('video,img', () => deleteElements(['video', 'img']));
+})();
